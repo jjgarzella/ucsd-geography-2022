ea1d7da4f78edaabef128d14d6c6fc855cecdf7e
Ryan Mike <rmike@ucsd.edu>
2022-05-28 17:29:29 -0700

    added formula for weighted CIs and plotting

--- CompleteIntersections.jl
+++ CompleteIntersections.jl
@@ -5,14 +5,35 @@ function classic_projective_ci_chern(n,D)
     error("Did not intersect with n-2 hyperplanes")
   end 
   Πdᵢ = prod(D)
-  println(Πdᵢ)
   nD = length(D)
   Σdᵢdⱼ = sum(D[i]*D[j] for i in 1:nD for j in 1:nD if i ≤ j) 
-  println(binomial(n+1,2))
+
   c₁_sq = Πdᵢ * (sum(D) - (n + 1))^2
   c₂ = Πdᵢ * ( Σdᵢdⱼ + binomial(n+1,2) - (n+1)*sum(D) ) 
+
   (c₁_sq, c₂)
 end
 
+function weighted_projective_ci_chern(A,D)
+  nA = length(A)
+  nD = length(D)
+  if nD != nA - 3
+    error("Did not intersect with n-2 hyperplanes")
+  end 
+  Πaᵢ = prod(A)
+  Σaᵢ = sum(A)
+  Πdᵢ = prod(D)
+  Σdᵢ = sum(D)
+  Σaᵢaⱼ = sum(A[i]*A[j] for i in 1:nD for j in 1:nA if i < j) 
+  Σdᵢdⱼ = sum(D[i]*D[j] for i in 1:nD for j in 1:nD if i ≤ j)
+  Σaᵢdⱼ = sum(A[i]*D[j] for i in 1:nA for j in 1:nD)
+
+  c₁_sq = (Σdᵢ - Σaᵢ)^2 * (Πdᵢ) / Πaᵢ
+  c₂ = (Σdᵢdⱼ + Σaᵢaⱼ + Σaᵢdⱼ) * (Πdᵢ) / Πaᵢ
+  # c₁_sq = (Σdᵢ - Σaᵢ)^2 * (Πdᵢ) * lcm(A...)^(nA-1) / Πaᵢ
+  # c₂ = (Σdᵢdⱼ + Σaᵢaⱼ + Σaᵢdⱼ) * (Πdᵢ) * lcm(A...)^(nA-1) / Πaᵢ
+
+  (c₁_sq, c₂)
+end
 
 end#module
--- Experiments.jl
+++ Experiments.jl
@@ -1,7 +1,55 @@
 module Experiments
+using Plots
+include("CompleteIntersections.jl")
 
-function test()
-    
+function classic_plot(N, multidegrees)
+    # N = int which is ambient dimension
+    # multidegrees = array of multidegrees
+    c1_sq_array = []
+    c2_array = []
+    for md in multidegrees
+        push!(c1_sq_array, CompleteIntersections.classic_projective_ci_chern(N, md)[1])
+        push!(c2_array, CompleteIntersections.classic_projective_ci_chern(N, md)[2])
+    end
+
+    plot(c1_sq_array,
+    c2_array,
+    seriestype = :scatter,
+    title = "P^$N, multidegrees in $multidegrees",
+    xaxis = "c_1^2",
+    yaxis = "c_2")
+end
+
+function weighted_plot(weights, multidegrees)
+    # weights = tuple of ints which are weights
+    # multidegrees = array of multidegrees
+    c1_sq_array = []
+    c2_array = []
+    for md in multidegrees
+        push!(c1_sq_array, CompleteIntersections.weighted_projective_ci_chern(weights, md)[1])
+        push!(c2_array, CompleteIntersections.weighted_projective_ci_chern(weights, md)[2])
+    end
+
+    plot(c1_sq_array,
+    c2_array,
+    seriestype = :scatter,
+    title = "P$weights, multidegrees in $multidegrees",
+    xaxis = "c_1^2",
+    yaxis = "c_2")
+end
+
+function weighted_plot_example()
+    weights = (1,1,2,7)
+    multidegrees = 1:18
+
+    weighted_plot(weights, multidegrees)
+end
+
+function classic_plot_example()
+    N = 3
+    multidegrees = 1:18
+
+    classic_plot(N, multidegrees)
 end
 
 end#module
